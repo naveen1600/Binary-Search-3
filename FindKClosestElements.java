9fbde239e1b87f5854a9f874ce6ea960d0da5eb0
Naveen Mathew Georgi <naveengeorgius@gmail.com>
2024-08-04 14:35:46 -0500

    Done

---
+++ FindKClosestElements.java
@@ -0,0 +1,27 @@
+// Time Complexity: O(log n)
+// Space Complexity: O(k)
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int n = arr.length;
+        int low = 0;
+        int high = n - k;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int distS = x - arr[mid];
+            int distE = arr[mid + k] - x;
+            if (distS > distE)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = low; i < low + k; i++)
+            result.add(arr[i]);
+
+        return result;
+
+    }
+}
---
+++ Pow(x,n).java
@@ -0,0 +1,22 @@
+// Time Complexity: O(n)
+// Space Complexity: O(log n)
+
+class Solution {
+    public double myPow(double x, int n) {
+        // base
+        if (n == 0)
+            return 1;
+
+        // logic
+        double result = myPow(x, n / 2);
+        if (n % 2 == 0)
+            return result * result;
+        else {
+            if (n < 0)
+                return result * result * 1 / x;
+            else
+                return result * result * x;
+        }
+
+    }
+}
